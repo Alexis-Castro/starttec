5c44a70f02a04c4022e7de0138fb66a6627db7cd
Alexis Castro <castropuse@gmail.com>
2026-07-07 20:52:29 -0500

    boton cerrar sesión

--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -1,4 +1,4 @@
-<aside class="fixed flex hidden left-0 top-0 z-20 lg:flex h-full w-64 lg:w-64 shrink-0 flex-col pt-16 duration-75 ease-in-out transition-[width] bg-[#1f2937]" id="nav-sidebar">
+<aside class="fixed hidden left-0 top-0 z-20 lg:flex h-full w-64 lg:w-64 shrink-0 flex-col pt-16 duration-75 ease-in-out transition-[width] bg-[#1f2937]" id="nav-sidebar">
    <!-- SIDEBAR HEADER -->
    <div class="relative flex min-h-0 flex-1 flex-col border-r border-[rgb(55,65,81)] pt-0">
       <div class="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
@@ -170,6 +170,15 @@
                      </a>
                   </li>
                   <!-- Menu Item Usuarios -->
+
+                  <li>
+                     <a class="group relative flex items-center gap-2.5 rounded-lg p-2 font-medium duration-300 ease-in-out hover:bg-gray-700 text-[#00C3FF]" href="/auth/logout">
+                        <svg class=" group-hover:fill-gray-300 fill-current" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                           <path fill-rule="evenodd" clip-rule="evenodd" d="M15.1007 19.247C14.6865 19.247 14.3507 18.9112 14.3507 18.497L14.3507 14.245H12.8507V18.497C12.8507 19.7396 13.8581 20.747 15.1007 20.747H18.5007C19.7434 20.747 20.7507 19.7396 20.7507 18.497L20.7507 5.49609C20.7507 4.25345 19.7433 3.24609 18.5007 3.24609H15.1007C13.8581 3.24609 12.8507 4.25345 12.8507 5.49609V9.74501L14.3507 9.74501V5.49609C14.3507 5.08188 14.6865 4.74609 15.1007 4.74609L18.5007 4.74609C18.9149 4.74609 19.2507 5.08188 19.2507 5.49609L19.2507 18.497C19.2507 18.9112 18.9149 19.247 18.5007 19.247H15.1007ZM3.25073 11.9984C3.25073 12.2144 3.34204 12.4091 3.48817 12.546L8.09483 17.1556C8.38763 17.4485 8.86251 17.4487 9.15549 17.1559C9.44848 16.8631 9.44863 16.3882 9.15583 16.0952L5.81116 12.7484L16.0007 12.7484C16.4149 12.7484 16.7507 12.4127 16.7507 11.9984C16.7507 11.5842 16.4149 11.2484 16.0007 11.2484L5.81528 11.2484L9.15585 7.90554C9.44864 7.61255 9.44847 7.13767 9.15547 6.84488C8.86248 6.55209 8.3876 6.55226 8.09481 6.84525L3.52309 11.4202C3.35673 11.5577 3.25073 11.7657 3.25073 11.9984Z" fill=""></path>
+                        </svg>
+                        cerrar sesión
+                     </a>
+                  </li>
                </ul>
             </nav>
             <!-- Sidebar Menu -->
